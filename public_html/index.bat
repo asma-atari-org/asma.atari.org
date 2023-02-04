09c342f83eb826d2028bcb0cb5925c947e2afcfc
Peter Dell <peter.dell@web.de>
2023-02-04 21:37:59 +0100

    Add lauch configuration

--- public_html/index.bat
+++ public_html/index.bat
@@ -1,2 +1,5 @@
-@rem Open Chrome with access to local files. This only works if not Chrome window is open yet.
-"%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" --process-per-site --allow-file-access-from-files "%~dp0%index.html"
+@echo off
+rem Close all open Chrome windows.
+taskkill /F /IM chrome* /T
+rem Open Chrome with access to local files. This only works if not Chrome window is open yet.
+"%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" --process-per-site --allow-file-access-from-files --disable-session-crashed-bubble "%~dp0%index.html"
