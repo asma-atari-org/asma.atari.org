51a7dfaf9a43de816adbf62d375695d2ac849af0
Peter Dell <peter.dell@web.de>
2025-12-20 19:18:18 +0100

    Update .bat scripts.
    Completeness and error handling.

--- bat/checkout.bat
+++ bat/checkout.bat
@@ -1,8 +1,9 @@
 @echo off
-rem ASAP Wiki and SVN are at http://asma.scene.pl
-rem SVN download is available at https://tortoisesvn.net/downloads.html
+rem ASAP Wiki and SVN are at "http://asma.scene.pl".
+rem SVN download is available at "https://tortoisesvn.net/downloads.html".
+rem Ensure to select the "Command Line Tools" during the installation, to install "svn.exe".
 rem This script assumes that "svn.exe" is in the path.
-rem Global SVN configuration is stored in %APPDATA%\Subversion\
+rem Global SVN configuration is stored in "%APPDATA%\Subversion\".
 rem Make sure to configure using commit time as file date/time.
 rem SVN content is checked out into a separate folder structure to separate SVN and Git versioning.
 rem A symbolic link is created for the "asma" folder in "public_html/asma" to include it into the website upload.
@@ -33,6 +34,8 @@ echo ASMA - Atari SAP Music Archive >%SVN_ASMA_TEXT_FILE%
 echo https://asma.atari.org>>%SVN_ASMA_TEXT_FILE%
 
 rem Checkout current ASMA revision.
+rem TortoiseSVN has the SVN command line client tools included in the download,
+rem but you must explicitly select them during installation.
 svn checkout svn://asma.scene.pl/asma/trunk >>%SVN_ASMA_TEXT_FILE%
 if ERRORLEVEL 1 (
   echo ERROR: SVN from https://tortoisesvn.net/downloads.html must be in the path.
@@ -41,13 +44,26 @@ if ERRORLEVEL 1 (
 echo Checked out on %DATE%. >>%SVN_ASMA_TEXT_FILE%
 type %SVN_ASMA_TEXT_FILE%
 
+
 rem Creating missing directory junctions does not require you to be an administrator
-if not exist %SITE_ASMA_FOLDER%. (
-  mklink /J %SITE_ASM_FOLDER% %SVN_TRUNK_ASMA_FOLDER%
+if not exist %SITE_HTML_FOLDER% (
+  mkdir %SITE_HTML_FOLDER%
 )
+if not exist %SITE_ASMA_FOLDER% (
+  mklink /J %SITE_ASMA_FOLDER% %SVN_TRUNK_ASMA_FOLDER%
+  if ERRORLEVEL 1 (
+    echo ERROR: Creation of preview junction failed. 
+    exit /b 1
+  )
+)
+
 rem Create directory junction, so ASMA can also be viewed in local XAMPP via http://127.0.0.1:8080/tmp/asma/public_html/
 if not exist %WUDSN_SITE_HTML_FOLDER%. (
   mklink /J %WUDSN_SITE_HTML_FOLDER% %SITE_HTML_FOLDER%
+  if ERRORLEVEL 1 (
+    echo ERROR: Creation of preview junction failed. 
+    exit /b 1
+  )
 )
 
 echo.
--- public_html/index.bat
+++ public_html/index.bat
@@ -5,4 +5,13 @@ echo Opening "%FILE%".
 rem Close all open Chrome windows.
 taskkill /F /IM chrome* /T >nul 2>nul
 rem Open Chrome with access to local files. This only works if no Chrome window is open yet.
-"%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" --process-per-site --allow-file-access-from-files --disable-session-crashed-bubble "%FILE%" 2>nul
+set CHROME=%ProgramFiles%\Google\Chrome\Application\chrome.exe
+if not exist "%CHROME%" goto :error_no_chrome
+
+"%CHROME%" --process-per-site --allow-file-access-from-files --disable-session-crashed-bubble "%FILE%" 2>nul
+if ERRORLEVEL 1 echo Could not start %CHROME%
+goto :eof
+
+:error_no_chrome
+echo ERROR: %CHROME% is not installed.
+exit /1
