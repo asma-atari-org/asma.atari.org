312c5f8564dc09545c576379fdd90bfbaa493f7b
Peter Dell <peter.dell@web.de>
2026-01-01 22:11:56 +0100

    Open both HTML pages

--- public_html/index.bat
+++ public_html/index.bat
@@ -1,14 +1,23 @@
 @echo off
 setlocal
-set FILE=%~dp0%index.html
-echo Opening "%FILE%".
+
 rem Close all open Chrome windows.
 taskkill /F /IM chrome* /T >nul 2>nul
+
 rem Open Chrome with access to local files. This only works if no Chrome window is open yet.
 set CHROME=%ProgramFiles%\Google\Chrome\Application\chrome.exe
 if not exist "%CHROME%" goto :error_no_chrome
 
-"%CHROME%" --process-per-site --allow-file-access-from-files --disable-session-crashed-bubble "%FILE%" 2>nul
+set FILE1=%~dp0%index.html
+set FILE2=%~dp0%asmadb\asmadb.html
+call :open_file
+
+goto :eof
+
+
+:open_file
+echo Opening "%FILE1%" and  "%FILE2%
+"%CHROME%" --process-per-site --allow-file-access-from-files --disable-session-crashed-bubble "%FILE1%" "%FILE2%" 
 if ERRORLEVEL 1 echo Could not start %CHROME%
 goto :eof
 
