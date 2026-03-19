929dc4d0dfdbd604b166410847120fe927c5d1f6
Peter Dell <peter.dell@web.de>
2026-03-20 00:28:55 +0100

    Add Fusion Builder for ASTIL

---
+++ java/src/org/atari/asma/stil/ASTIL.java
@@ -0,0 +1,13 @@
+// Generated automatically with "fut". Do not edit.
+package org.atari.asma.stil;
+
+public class ASTIL
+{
+
+	public static String getMessage()
+	{
+		final ASTILCover cover = new ASTILCover();
+		cover.comment = "Test2";
+		return cover.comment;
+	}
+}
---
+++ java/src/org/atari/asma/stil/ASTILCover.java
@@ -0,0 +1,11 @@
+// Generated automatically with "fut". Do not edit.
+package org.atari.asma.stil;
+
+class ASTILCover
+{
+	private int startSeconds;
+	private int endSeconds;
+	private String titleAndSource;
+	private String artist;
+	String comment;
+}
